27f5d2631299231e59db1ae68983c682dbce769e
wecaners <wecaners@naver.com>
2023-01-02 18:01:06 +0900

    Add:초기셋팅 완료

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Recipe from './pages/Main/Components/Recipe/Recipe';
+import Review from './pages/Main/Components/Review/Review';
+import Brand from './pages/Main/Components/Brand/Brand';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './pages/Main/Main';
+import ProductList from './pages/Main/Components/ProductList/ProductList';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    chilren: [
+      { index: true, path: '/', element: <Main /> },
+      { path: '/products', element: <ProductList /> },
+      { path: '/recipe', element: <Recipe /> },
+      { path: '/review', element: <Review /> },
+      { path: '/brand', element: <Brand /> },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
