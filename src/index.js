382d12af9a4025744c48ab70648ccf20d23da63c
dagyeong Jeong <jdk09140@gmail.com>
2023-01-03 15:31:30 +0900

    ADD : Router, index.js 추가

--- src/index.js
+++ src/index.js
@@ -1,33 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Main from './pages/Main/Main';
-import Recipe from './pages/Main/Components/Recipe/Recipe';
-import Review from './pages/Main/Components/Review/Review';
-import Brand from './pages/Main/Components/Brand/Brand';
-import ProductList from './pages/Main/Components/ProductList/ProductList';
-import './styles/common.scss';
+import router from './Router';
 import './styles/reset.scss';
-import './styles/variables.scss';
-import Login from './pages/Login/Login';
-import SignUp from './pages/SignUp/SignUp';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      { index: true, path: '/', element: <Main /> },
-      { path: '/products', element: <ProductList /> },
-      { path: '/recipe', element: <Recipe /> },
-      { path: '/review', element: <Review /> },
-      { path: '/brand', element: <Brand /> },
-      { path: '/login', element: <Login /> },
-      { path: '/signup', element: <SignUp /> },
-    ],
-  },
-]);
+import './styles/common.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
