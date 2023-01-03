0b801a7bab1353bc3bda6ac0ff956e47f8c0238f
wecaners <wecaners@naver.com>
2023-01-03 13:47:20 +0900

    Modify: 수정사항 반영

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './styles/common.scss';
-import './styles/reset.scss';
-import './styles/variables.scss';
 import router from './Router';
+import './styles/reset.scss';
+import './styles/common.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
