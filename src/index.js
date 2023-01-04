9e36a1271d3804f866208b41c78b122ca1463bfb
kwonnahyun <skgusskgus2@naver.com>
2023-01-04 17:52:05 +0900

    detail page 수정

--- src/index.js
+++ src/index.js
@@ -1,29 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Main from './pages/Main/Main';
-import Recipe from './pages/Main/Components/Recipe/Recipe';
-import Review from './pages/Main/Components/Review/Review';
-import Brand from './pages/Main/Components/Brand/Brand';
-import ProductList from './pages/Main/Components/ProductList/ProductList';
+import { RouterProvider } from 'react-router-dom';
 import './styles/common.scss';
 import './styles/reset.scss';
-import './styles/variables.scss';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      { index: true, path: '/', element: <Main /> },
-      { path: '/products', element: <ProductList /> },
-      { path: '/recipe', element: <Recipe /> },
-      { path: '/review', element: <Review /> },
-      { path: '/brand', element: <Brand /> },
-    ],
-  },
-]);
+import router from './Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
