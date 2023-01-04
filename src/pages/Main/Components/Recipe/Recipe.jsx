75ef748fc37aa15bcc833e76ae4be96b992742f5
wecaners <wecaners@naver.com>
2023-01-04 23:08:50 +0900

    Add: 레시피 페이지 레이아웃 추가

--- src/pages/Main/Components/Recipe/Recipe.jsx
+++ src/pages/Main/Components/Recipe/Recipe.jsx
@@ -4,7 +4,7 @@ import './Recipe.scss';
 export default function Recipe() {
   return (
     <div className="recipe-total-container">
-      <div className="topVisualBox">
+      <div className="top-visual-box">
         <p className="title">Recipe</p>
       </div>
       <div className="recipe-navigation">
@@ -17,9 +17,10 @@ export default function Recipe() {
                     <div className="img-box">
                       <div className="img">
                         <img
-                          src="https://images.vexels.com/media/users/3/130179/isolated/preview/d9afcd8a07b20f34489e8b649d43d6d7-eggplant-flat-circle-icon.png"
+                          src="https://www.osulloc.com/upload/kr/ko/adminImage/ON/LI/20220518005721019DU.png"
                           alt="레시피 추천"
                         />
+                        <p className="text">Leachou추천</p>
                       </div>
                     </div>
                   </div>
@@ -29,9 +30,10 @@ export default function Recipe() {
                     <div className="img-box">
                       <div className="img">
                         <img
-                          src="https://images.vexels.com/media/users/3/130179/isolated/preview/d9afcd8a07b20f34489e8b649d43d6d7-eggplant-flat-circle-icon.png"
+                          src="https://www.osulloc.com/upload/kr/ko/adminImage/ZB/DD/20220518005819370VJ.png"
                           alt="베스트"
                         />
+                        <p className="text">베스트</p>
                       </div>
                     </div>
                   </div>
@@ -41,9 +43,10 @@ export default function Recipe() {
                     <div className="img-box">
                       <div className="img">
                         <img
-                          src="https://images.vexels.com/media/users/3/130179/isolated/preview/d9afcd8a07b20f34489e8b649d43d6d7-eggplant-flat-circle-icon.png"
+                          src="https://www.osulloc.com/upload/kr/ko/adminImage/ZH/XS/20220518005913180YI.png"
                           alt="부담없는 선물"
                         />
+                        <p className="text">부담없는 선물</p>
                       </div>
                     </div>
                   </div>
@@ -53,9 +56,10 @@ export default function Recipe() {
                     <div className="img-box">
                       <div className="img">
                         <img
-                          src="https://images.vexels.com/media/users/3/130179/isolated/preview/d9afcd8a07b20f34489e8b649d43d6d7-eggplant-flat-circle-icon.png"
+                          src="https://www.osulloc.com/upload/kr/ko/adminImage/OJ/OD/20220913101158486TY.png"
                           alt="티웨어 세트"
                         />
+                        <p className="text">티웨어 세트</p>
                       </div>
                     </div>
                   </div>
@@ -76,6 +80,77 @@ export default function Recipe() {
               </p>
             </div>
             <div className="prd-list-wrap">
+              <img
+                className="prd-recommend-image1"
+                src="https://www.osulloc.com/upload/kr/ko/adminImage/IE/EF/304_20220120172110895WE_pc_large.png?quality=80"
+              />
+              <img
+                className="prd-recommend-image2"
+                src="https://www.osulloc.com/upload/kr/ko/adminImage/JY/QU/304_20210825170710069ZN_pc_large.png?quality=80"
+              />
+              <div className="swiper-container prd-slideban swiper-container-initialized swiper-container-horizontal" />
+            </div>
+          </div>
+        </section>
+        <section className="recipe-sec recipe-typeB">
+          <div className="w1200-2">
+            <div className="sec-tit">
+              <p className="text01">베스트</p>
+              <p className="text02">
+                Lechou에서 많이 선물된 선물세트를 모았어요.
+              </p>
+            </div>
+            <div className="prd-list-wrap">
+              <img
+                className="prd-recommend-image1"
+                src="https://www.osulloc.com/upload/kr/ko/adminImage/HC/LW/20201027143838409ZN.png?quality=80"
+              />
+              <img
+                className="prd-recommend-image2"
+                src="https://www.osulloc.com/upload/kr/ko/adminImage/KF/NE/20220401151823970TE.png?quality=80"
+              />
+              <div className="swiper-container prd-slideban swiper-container-initialized swiper-container-horizontal" />
+            </div>
+          </div>
+        </section>
+        <section className="recipe-sec recipe-typeB">
+          <div className="w1200-2">
+            <div className="sec-tit">
+              <p className="text01">부담없는 선물</p>
+              <p className="text02">
+                감사한 마음을 표현하고 싶을 때 부담 없이 선물해요.
+              </p>
+            </div>
+            <div className="prd-list-wrap">
+              <img
+                className="prd-recommend-image1"
+                src="https://www.osulloc.com/upload/kr/ko/adminImage/BN/PO/20220623175406184ER.png?quality=80"
+              />
+              <img
+                className="prd-recommend-image2"
+                src="https://www.osulloc.com/upload/kr/ko/adminImage/FX/HH/20221117161844223WC.png?quality=80"
+              />
+              <div className="swiper-container prd-slideban swiper-container-initialized swiper-container-horizontal" />
+            </div>
+          </div>
+        </section>
+        <section className="recipe-sec recipe-typeB">
+          <div className="w1200-2">
+            <div className="sec-tit">
+              <p className="text01">티웨어 세트</p>
+              <p className="text02">
+                티 입문자에게 선물하기 좋은 티웨어 세트 추천드려요.
+              </p>
+            </div>
+            <div className="prd-list-wrap">
+              <img
+                className="prd-recommend-image1"
+                src="https://www.osulloc.com/upload/kr/ko/adminImage/FX/HH/20221117161844223WC.png?quality=80"
+              />
+              <img
+                className="prd-recommend-image2"
+                src="https://www.osulloc.com/upload/kr/ko/adminImage/XK/EC/20220623175706855SY.png?quality=80"
+              />
               <div className="swiper-container prd-slideban swiper-container-initialized swiper-container-horizontal" />
             </div>
           </div>
