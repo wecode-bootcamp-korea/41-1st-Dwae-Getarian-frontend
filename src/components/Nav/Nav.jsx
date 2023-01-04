3b7dab68cf9c79f4eeb983d79d24f181814ab772
wecaners <wecaners@naver.com>
2023-01-04 10:17:36 +0900

    Modify: css파일 수정

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,38 +7,40 @@ import './Nav.scss';
 
 export default function Nav() {
   return (
-    <div className="nav-wrapper">
-      <div className="nav-left-box">
-        <Link className="nav-left-box-main-btn" to="/">
-          Lechou
-        </Link>
-        <Link className="nav-left-box-product-btn" to="/main/product">
-          제품
-        </Link>
-        <Link className="nav-left-box-recipe-btn" to="/main/recipe">
-          레시피
-        </Link>
-        <Link className="nav-left-box-review-btn" to="/main/review">
-          리뷰
-        </Link>
-        <Link className="nav-left-box-brand-btn" to="/main/brand">
-          브랜드
-        </Link>
-      </div>
-      <div className="nav-right-box">
-        <Link Link to="/search">
-          <CiSearch />
-        </Link>
-        <Link Link to="/cart">
-          <CiShoppingCart />
-        </Link>
-        <Link Link to="/delivery">
-          <CiDeliveryTruck />
-        </Link>
-        <Link Link to="/main/login">
-          로그인
-        </Link>
-        <Link to="/main/signup">signup</Link>
+    <div className="nav-header">
+      <div className="nav-wrapper">
+        <div className="nav-left-box">
+          <Link className="nav-left-box-main-btn" to="/">
+            Lechou
+          </Link>
+          <Link className="nav-left-box-btns" to="/main/product">
+            제품
+          </Link>
+          <Link className="nav-left-box-btns" to="/main/recipe">
+            레시피
+          </Link>
+          <Link className="nav-left-box-btns" to="/main/review">
+            리뷰
+          </Link>
+          <Link className="nav-left-box-btns" to="/main/brand">
+            브랜드
+          </Link>
+        </div>
+        <div className="nav-right-box">
+          <Link Link to="/search">
+            <CiSearch />
+          </Link>
+          <Link Link to="/cart">
+            <CiShoppingCart />
+          </Link>
+          <Link Link to="/delivery">
+            <CiDeliveryTruck />
+          </Link>
+          <Link Link to="/main/login">
+            로그인
+          </Link>
+          <Link to="/main/signup">signup</Link>
+        </div>
       </div>
     </div>
   );
