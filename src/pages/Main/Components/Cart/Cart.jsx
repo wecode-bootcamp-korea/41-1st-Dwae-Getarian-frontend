c7f072f67df5d6ebefb4a0e5168864b8962a64fe
kwonnahyun <skgusskgus2@naver.com>
2023-01-05 15:46:02 +0900

    ADD: cart 페이지 레이아웃 추가

--- src/pages/Main/Components/Cart/Cart.jsx
+++ src/pages/Main/Components/Cart/Cart.jsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import Payment from '../Product/components/payment/Payment';
+import './cart.scss';
+import CartList from './CartList/CartList';
 
 export default function Cart() {
-  return <div>Cart</div>;
+  return (
+    <div className="cartContainer">
+      <div className="cart">
+        <h1>장바구니</h1>
+        <div className="cartBox">
+          <div className="cartBoxTop">
+            <div className="cartBoxCheck">
+              <input type="checkbox" id="checkbox" />
+              <label htmlFor="checkbox">전체선택</label>
+            </div>
+            <div className="cartBoxDelete">
+              <button>선택 삭제</button>
+            </div>
+          </div>
+
+          <CartList />
+          <div className="cartBtn">
+            <button className="selectBtn">선택상품 주문</button>
+            <button className="totalBtn">전체상품 주문</button>
+          </div>
+        </div>
+      </div>
+      <div className="payment">
+        <Payment />
+      </div>
+    </div>
+  );
 }
