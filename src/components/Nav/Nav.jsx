6ce35143c167ee4a2d414c656efdf143c798c8d7
wecaners <wecaners@naver.com>
2023-01-10 15:11:27 +0900

    Modify: css 수정

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -23,101 +23,92 @@ export default function Nav() {
   return (
     <div className="nav-header">
       <div className={scrollPosition < 200 ? 'nav-original' : 'nav-changed'}>
-        <div className="nav-hover">
-          <div className="nav-wrapper">
-            <div className="nav">
-              <div className="nav-left-box">
-                <Link className="nav-left-box-main-btn nav-btns" to="/">
-                  Lechou
-                </Link>
-                <div className="nav-left-box-btn">
-                  <ul className="nav-list-dep1">
-                    <li className="nav-dep1-item">
-                      <Link
-                        className="nav-left-box-btns nav-btns"
-                        to="/main/product"
-                      >
-                        제품
-                      </Link>
-                      <div class="nav-list-dep2-box">
-                        <div className="wrapper">
-                          <div className="nav-flex-box">
-                            <ul className="nav-list-dep2">
-                              <li className="nav-dep2-item" />
-                              <li className="nav-dep2-item">깊이 2 아이템</li>
-                              <li className="nav-dep2-item">깊이 2 아이템</li>
-                              <li className="nav-dep2-item">깊이 2 아이템</li>
-                              <li className="nav-dep2-item">깊이 2 아이템</li>
-                            </ul>
+        <div className="vacant-container">
+          <div className="nav-hover">
+            <div className="nav-wrapper">
+              <div className="nav">
+                <div className="nav-left-box">
+                  <Link className="nav-left-box-main-btn nav-btns" to="/">
+                    Lechou
+                  </Link>
+                  <div className="nav-left-box-btn">
+                    <ul className="nav-list-dep1">
+                      <li className="nav-dep1-item">
+                        <Link
+                          className="nav-left-box-btns nav-btns"
+                          to="/main/best"
+                        >
+                          베스트
+                        </Link>
+                        <div class="nav-list-dep2-box">
+                          <div className="wrapper">
+                            <div className="nav-flex-box" />
                           </div>
                         </div>
-                      </div>
-                    </li>
-                    <li className="nav-dep1-item">
-                      <Link
-                        className="nav-left-box-btns nav-btns"
-                        to="/main/recipe"
-                      >
-                        레시피
-                      </Link>
-                    </li>
-                    <li className="nav-dep1-item">
-                      <Link
-                        className="nav-left-box-btns nav-btns"
-                        to="/main/review"
-                      >
-                        리뷰
-                      </Link>
-                    </li>
-                    <li className="nav-dep1-item">
-                      <Link
-                        className="nav-left-box-btns nav-btns"
-                        to="/main/brand"
-                      >
-                        브랜드
-                      </Link>
-                    </li>
-                  </ul>
+                      </li>
+                      <li className="nav-dep1-item">
+                        <Link
+                          className="nav-left-box-btns nav-btns"
+                          to="/main/product"
+                        >
+                          제품
+                        </Link>
+                      </li>
+                      <li className="nav-dep1-item">
+                        <Link
+                          className="nav-left-box-btns nav-btns"
+                          to="/main/recipe"
+                        >
+                          선물추천
+                        </Link>
+                      </li>
+                      <li className="nav-dep1-item">
+                        <Link className="nav-left-box-btns nav-btns">
+                          브랜드
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
-              </div>
-              <div className="nav-right-box">
-                <div className="nav-right-box-icons">
+                <div className="nav-right-box">
+                  <div className="nav-right-box-icons">
+                    <Link
+                      className="nav-right-box-icons-search nav-right-box-icon-group nav-btns"
+                      Link
+                      to="/search"
+                    >
+                      <CiSearch />
+                    </Link>
+                    <Link
+                      className="nav-right-box-icons-cart nav-right-box-icon-group nav-btns"
+                      Link
+                      to="/cart"
+                    >
+                      <CiShoppingCart />
+                    </Link>
+                    <Link
+                      className="nav-right-box-icons-truck nav-right-box-icon-group nav-btns"
+                      Link
+                      to="/delivery"
+                    >
+                      <CiDeliveryTruck />
+                    </Link>
+                  </div>
                   <Link
-                    className="nav-right-box-icons-search nav-right-box-icon-group nav-btns"
+                    className="nav-right-box-login nav-btns hovered-login-signup-btns"
                     Link
-                    to="/search"
+                    to="/main/login"
                   >
-                    <CiSearch />
+                    로그인
                   </Link>
                   <Link
-                    className="nav-right-box-icons-cart nav-right-box-icon-group nav-btns"
+                    className="nav-right-box-signup nav-btns hovered-login-signup-btns"
                     Link
-                    to="/cart"
+                    to="/main/signup"
                   >
-                    <CiShoppingCart />
-                  </Link>
-                  <Link
-                    className="nav-right-box-icons-truck nav-right-box-icon-group nav-btns"
-                    Link
-                    to="/delivery"
-                  >
-                    <CiDeliveryTruck />
+                    회원가입
                   </Link>
                 </div>
-                <Link
-                  className="nav-right-box-login nav-btns hovered-login-signup-btns"
-                  Link
-                  to="/main/login"
-                >
-                  로그인
-                </Link>
-                <Link
-                  className="nav-right-box-signup nav-btns hovered-login-signup-btns"
-                  Link
-                  to="/main/signup"
-                >
-                  회원가입
-                </Link>
               </div>
             </div>
           </div>
