9955c4189a6f416c371c46a3fbfccc3ea32c8eac
kwonnahyun <skgusskgus2@naver.com>
2023-01-11 17:40:58 +0900

    conflict 해결

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './Router';
-import './styles/variables.scss';
+import './styles/common.scss';
+import './styles/reset.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Router />);
